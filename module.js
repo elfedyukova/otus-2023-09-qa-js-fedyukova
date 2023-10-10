16cbcfdae1e35155600f93955caf4937730290fd
elfedyukova <elfedyukova@yandex.ru>
2023-10-10 18:37:46 +0300

    work with js

---
+++ module.js
@@ -0,0 +1,7 @@
+export function hello (name) {
+    console.log('Hello, ', name)
+}
+
+export const greet = (username) => {
+    console.info(username)
+}
---
+++ test.js
@@ -0,0 +1,4 @@
+import { hello, greet } from './module.js'
+
+hello('Elena')
+greet('Hi')
